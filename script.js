2715ee37b6b751b389cbbfd300d431a70f0786f9
SRBond <skyler.bond7@gmail.com>
2020-01-07 11:07:07 -0800

    Laid out some HTML/CSS

---
+++ script.js
@@ -0,0 +1,22 @@
+//select HTML elements (quadrants, start button, reset button, loss overlay, win overlay)
+
+//add event listeners for start, reset, and quadrants
+
+//start handler
+    //if game not already in progress
+        //start game
+    //else
+        //reset game state
+
+//reset handler
+    //reset game state to beginning
+
+//quadrant handler
+    //for each iteration of the game
+    //count number of quadrants clicked in this round
+    for(i=0; i<iterations; i++){
+        //if quadrant clicked is equal to current quadrant per count
+            //success, move on
+        // else if quadrant is incorrect
+            //display loss overlay and reset game state
+    }

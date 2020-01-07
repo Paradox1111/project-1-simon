e48bf1bd63a9e9f15b4205b45eec8265b6d40fe3
SRBond <skyler.bond7@gmail.com>
2020-01-07 12:00:37 -0800

    Finished basic CSS and started on script

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
+//Starting game state
+let quadrantArr = [];
+let round = 0;
+let clickCounter = 0;
+let score = 0;
 //select HTML elements (quadrants, start button, reset button, loss overlay, win overlay)
 
 //add event listeners for start, reset, and quadrants
 
-//start handler
-    //if game not already in progress
-        //start game
-    //else
-        //reset game state
-
-//reset handler
-    //reset game state to beginning
-
-//quadrant handler
-    //for each iteration of the game
-    //count number of quadrants clicked in this round
-    for(i=0; i<iterations; i++){
-        //if quadrant clicked is equal to current quadrant per count
-            //success, move on
-        // else if quadrant is incorrect
-            //display loss overlay and reset game state
+function handleQuadrant(){
+    //if quadrant clicked is equal to current quadrant 
+    if(quadrantArr[round] === evt.target){
+        //success, increment clickCounter
+        //if clickCounter >== round
+            //increment round
+            //call quadrantRandomizer
+    } else { //if quadrant clicked does not match
+        //display loss overlay and 
+        handleReset()
     }
+}
+
+//quadrant randomizer
+    //once for each round (i.e. only once on the first round)
+        //illuminate a random quadrant and 
+        //append that quadrant to the quadrantArr for later comparison
+    //After each quadrant has been illuminated and appended:
+    //prompt user to reiterate the sequence
+
+//start btn handler
+function handleStart(){
+    //if game not already in progress
+    if(round >= 1){
+        //start game with quadrant randomizer
+        quadrantRandomizer()
+    } else {
+        //alert user to game in progress
+        alert("Game in progress, select quit if you'd like to restart")
+    }
+}
+
+//quit btn handler
+function handleReset(){
+    //reset game state to default values
+    clickCounter = 0;
+    score = 0;
+    quadrantArr = [];
+    round = 1;
+}
